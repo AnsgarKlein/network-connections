31047f8f5a0de27786e2e422afa71687eb6fa303
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2024-10-22 20:17:10 +0200

    Switch from parsing netstat to reading kernel files
    Instead of parsing netstat command which is prone to failure because
    different versions of netstat dont get their columns alignment right in
    different ways.

--- network_connections.py
+++ network_connections.py
@@ -24,200 +24,267 @@
 
 
 """
-This script uses the netstat command line tool to gather a list of active
-connections. This information is parsed and categorized by connection protocol
-and connection state.
-It is then printed out in JSON format.
+This script reads connection info from virtual kernel files /proc/net/tcp,
+/proc/net/tcp6, /proc/net/udp, /proc/net/udp6 similar to the netstat command.
+
+This information is parsed and categorized by connection protocol and
+connection state. It is then printed out in JSON format.
 """
 
-
 import json
-import os
-import shutil
-import subprocess
+import re
 import sys
 
 
-def check_netstat_availability(): # type: () -> bool
+# Map from state enum number to string
+# Taken from Linux kernel source at /include/net/tcp_states.h
+STATE_MAP = {
+    1: 'ESTABLISHED',
+    2: 'SYN_SENT',
+    3: 'SYN_RECV',
+    4: 'FIN_WAIT1',
+    5: 'FIN_WAIT2',
+    6: 'TIME_WAIT',
+    7: 'CLOSE',
+    8: 'CLOSE_WAIT',
+    9: 'LAST_ACK',
+    10: 'LISTEN',
+    11: 'CLOSING',
+    12: 'UNKNOWN',  # Actually TCP_NEW_SYN_RECV
+    13: 'UNKNOWN',  # Actually BOUND_INACTIVE
+}
+
+
+def get_raw_connection_data(): # type: () -> tuple
     """
-    Check if netstat is installed and can be found in PATH.
+    Read kernel files and return tuple of contents.
 
-    :return: True if netstat is in PATH, false otherwise
+    Read virtual files /proc/net/tcp, /proc/net/tcp6, /proc/net/udp,
+    /proc/net/udp6 remove the first line (contains header) and split them on
+    newline character.
+    Return tuple of list of line strings.
+    Every file is returned in a separate list.
+
+    :return: 4-Tuple of list of lines of four files
     """
 
-    path = shutil.which('netstat')
-    return path is not None
+    def get_lines_of_file(path): # type: (str) -> list
+        with open(path, encoding='ascii') as f:
+            content = f.read()
+            lines = content.split('\n')
+            lines = [l.strip() for l in lines]
+            lines = [l for l in lines if l != '']
+            return lines
 
-def run_netstat(): # type: () -> str
+    tcp4 = get_lines_of_file('/proc/net/tcp')
+    tcp6 = get_lines_of_file('/proc/net/tcp6')
+    udp4 = get_lines_of_file('/proc/net/udp')
+    udp6 = get_lines_of_file('/proc/net/udp6')
+
+    # Skip headers
+    tcp4 = tcp4[1:]
+    tcp6 = tcp6[1:]
+    udp4 = udp4[1:]
+    udp6 = udp6[1:]
+
+    return tcp4, tcp6, udp4, udp6
+
+def parse_raw_connection_data(tcp4, tcp6, udp4, udp6): # type: (list, list, list, list) -> list
     """
-    Run netstat command and return stdout.
+    Parse given list of TCP 4/6, UDP 4/6 lines and return result as list of dicts.
 
-    :raises Exception: When experiencing problems with netstat command
-    :return: Stdout of netstat command
+    :param tcp4: List of tcp4 lines
+    :param tcp6: List of tcp6 lines
+    :param udp4: List of udp4 lines
+    :param udp6: List of udp6 lines
+    :return: List of dictionaries containing info from all given strings
     """
 
-    netstat_cmd = [
-        'netstat',
-        '--numeric',
-        '--wide',
-        '--protocol=inet,inet6'
+    # Regular expression pattern for extracting fields from string
+    tcp_pattern_str = [
+        r'^',
+        r'([0-9]+):',                          # "sl" column
+        r'\s+',
+        r'([0-9A-Fa-f]{32}|[0-9A-Fa-f]{8})',   # "local_address" column (ip only)
+        r':',                                  # "local_address" column delimiter
+        r'([0-9A-Fa-f]{32}|[0-9A-Fa-f]{4})',   # "local_address" column (port only)
+        r'\s+',
+        r'([0-9A-Fa-f]{32}|[0-9A-Fa-f]{8})',   # "rem_address" column (ip only)
+        r':',                                  # "rem_address" column delimiter
+        r'([0-9A-Fa-f]{32}|[0-9A-Fa-f]{4})',   # "rem_address" column (port only)
+        r'\s+',
+        r'([0-9A-Fa-f]{2})',                   # "st" column
+        r'\s+',
+        r'([0-9A-Fa-f]+)',                     # "tx_queue" column
+        r':',
+        r'([0-9A-Fa-f]+)',                     # "rx_queue" column
+        r'\s+',
+        r'([0-9A-Fa-f]+)',                     # "tr" column
+        r':',
+        r'([0-9A-Fa-f]+)',                     # "tm->when" column
+        r'\s+',
+        r'([0-9A-Fa-f]+)',                     # "retrnsmt" column
+        r'\s+',
+        r'([0-9]+)',                           # "uid" column
+        r'\s+',
+        r'([0-9]+)',                           # "timeout" column
+        r'\s+',
+        r'([0-9]+)',                           # "inode" column
+        r'\s+',
+        r'(.*)$',                              # ??
     ]
 
-    environ = os.environ.copy()
-    environ['LC_ALL'] = 'en_US'
+    tcp_pattern = re.compile(''.join(tcp_pattern_str))
 
-    cmd = subprocess.run(
-        netstat_cmd,
-        env=environ,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-        check=True)
+    def parse_line(line): # type: (str) -> dict
+        # Match regular expression
+        match = re.match(tcp_pattern, line)
 
-    if cmd.returncode != 0:
-        raise Exception(f'Error running netstat: {cmd.stderr}')
+        # Error if line did not match
+        if match is None:
+            raise Exception(('Could not extract info from line:\n'
+                             f'{line}'))
 
-    return cmd.stdout
+        # Extract fields
+        entry = {}
+        entry['sl']            = match.groups()[0]
+        entry['local_address'] = match.groups()[1]
+        entry['local_port']    = match.groups()[2]
+        entry['rem_address']   = match.groups()[3]
+        entry['rem_port']      = match.groups()[4]
+        entry['st']            = match.groups()[5]
+        entry['tx_queue']      = match.groups()[6]
+        entry['rx_queue']      = match.groups()[7]
+        entry['tr']            = match.groups()[8]
+        entry['tm->when']      = match.groups()[9]
+        entry['retrnsmt']      = match.groups()[10]
+        entry['uid']           = match.groups()[11]
+        entry['timeout']       = match.groups()[12]
+        entry['inode']         = match.groups()[13]
 
-def tokenize_netstat_headers(headers_line, all_lines): # type: (str, list) -> list
+        return entry
+
+    # Parse all TCP 4/6 and UDP 4/6 lines and add protocol info
+    connections = []
+    for line in tcp4:
+        entry = parse_line(line)
+        entry['proto'] = 'tcp4'
+        connections.append(entry)
+    for line in tcp6:
+        entry = parse_line(line)
+        entry['proto'] = 'tcp6'
+        connections.append(entry)
+    for line in udp4:
+        entry = parse_line(line)
+        entry['proto'] = 'udp4'
+        connections.append(entry)
+    for line in udp6:
+        entry = parse_line(line)
+        entry['proto'] = 'udp6'
+        connections.append(entry)
+
+    return connections
+
+def post_process_connection_data(connections): # type: (list) -> list
     """
-    Tokenize header line of netstat output.
+    Post-process already parsed connection information.
 
-    :param headers_line: The string containing all the headers
-    :param all_lines: List of all lines of netstat output (headers + data lines)
-    :return: List of header strings in correct order
+    Change hex encoded IPv4 and IPv6 addresses to normal
+    quad octets / hexadecimal colon-separated format.
+    Change hex encoded local and remote port to decimal.
+    Remove entries with 0.0.0.0 / 0000:0000:0000:0000:0000:0000:0000:0000
+    set as remote address.
+    Convert connection state from hex identifier to string value.
+    (Copies data. Does not modify input.)
+
+    :param connections: List of dictionaries describing connections
+    :return: Post-processed copy of input list
     """
-
-    def is_whitespace_column(index): # type: (int) -> bool
-        for line in all_lines:
-            # Index is not on this line
-            if len(line) < index:
-                return False
-
-            if line[index] not in [' ', '\\t']:
-                return False
-
-
-        return True
-
-    headers = []
-    current_header = ''
-    for i, c in enumerate(headers_line):
-        if c not in [' ', '\\t']:
-            current_header += c
-        else:
-            # Check if whitespace may be end of header string
-            if is_whitespace_column(i):
-                # If we finished a header string add it to list of headers
-                if current_header != '':
-                    headers.append(current_header)
-                current_header = ''
-            else:
-                current_header += c
-    # Add last header
-    if current_header != '':
-        headers.append(current_header)
-
-    # Strip whitespaces from headers
-    headers = [h.strip() for h in headers]
-
-    return headers
-
-def tokenize_netstat_data(data_lines, headers): # type: (list, list) -> list
-    """
-    Tokenize list of data lines of netstat output.
-
-    :param data_lines: List of data lines of netstat output to tokenize
-    :param headers: List of headers for given data lines
-    :raises Exception: If info in data lines does not match given headers
-    :return: List of dictionaries containing information from given data lines
-    """
-
-    data_list = []
-
-    for line in data_lines:
-        columns = line.split(' ')
-        columns = [c for c in columns if c != '']
-
-        if len(columns) != len(headers):
-            raise Exception((f'Parsed more or less columns than expected in line: {line}.',
-                             f'Expected the values for the following: {", ".join(headers)}'))
-
-        # Create data entry with correct header names in correct order
-        data_entry = {header: columns[i] for i, header in enumerate(headers)}
-        data_list.append(data_entry)
-
-    return data_list
-
-def parse_netstat_output(stdout): # type: (str) -> list
-    """
-    Parse output of netstat command.
-
-    Output is a list of dictionaries. Each dictionary describes the content of
-    a single line of output. Keys in the dictionaries are the column names in
-    netstat output.
-
-    :param stdout: Stdout output of netstat command
-    :raises Exception: If given output does not contain enough lines
-    :return: List of dictionaries (one dict per line)
-    """
-
-    all_lines = stdout.strip().split('\n')
-    all_lines = [l.strip() for l in all_lines]
-
-    if len(all_lines) < 2:
-        raise Exception('Cannot parse netstat output, output does not contain enough lines')
-
-    # Ignore first line, it contains useless info
-    # Headers are in the second line, the rest is data
-    headers_line = all_lines[1]
-    data_lines = all_lines[2:]
-
-    # Get list of headers from header line
-    headers = tokenize_netstat_headers(headers_line, [headers_line] + data_lines)
-
-    # Parse data lines into list of dicts with help of header list
-    data_list = tokenize_netstat_data(data_lines, headers)
-    return data_list
-
-def post_process_data(data_list): # type: (list) -> list
-    """
-    Post process parsed netstat data.
-
-    Protocols "udp" and "tcp" are renamed to the more fitting name "udp4"
-    and "tcp4".
-    Copies data. Does not modify input.
-
-    :param data_list: List of dictionaries containing info from netstat
-    :raises Exception: If protocol column could not be found in dictionaries
-    :return: List of modified dictionaries
-    """
-
-    # Data entries are guaranteed to all have the same keys, so we
-    # just need to check in one of them.
-    if 'Proto' not in data_list[0]:
-        raise Exception('Could not find column "Proto" in netstat output')
 
     output = []
-    for entry in data_list:
-        copy = dict(entry)
 
-        if copy['Proto'] == 'tcp':
-            copy['Proto'] = 'tcp4'
-        elif copy['Proto'] == 'udp':
-            copy['Proto'] = 'udp4'
+    def hex_ip_to_decimal(text):
+        output = ''
+        if len(text) == 8:
+            # IPv4 address
+            # pylint: disable=consider-using-f-string
+            output = '{}.{}.{}.{}'.format(
+                int(text[6:8], 16),
+                int(text[4:6], 16),
+                int(text[2:4], 16),
+                int(text[0:2], 16))
+            return output
+        if len(text) == 32:
+            # IPv6 address
+            # pylint: disable=consider-using-f-string
+            output = '{}{}:{}{}:{}{}:{}{}:{}{}:{}{}:{}{}:{}{}'.format(
+                # Word 1
+                text[6:8],
+                text[4:6],
+                text[2:4],
+                text[0:2],
 
-        output.append(copy)
+                # Word 2
+                text[14:16],
+                text[12:14],
+                text[10:12],
+                text[8:10],
+
+                # Word 3
+                text[22:24],
+                text[20:22],
+                text[18:20],
+                text[16:18],
+
+                # Word 4
+                text[30:32],
+                text[28:30],
+                text[26:28],
+                text[24:26])
+            return output.lower()
+
+        raise Exception(f'Given value "{text}" does not look like an IP address!')
+
+    # Process every entry separately
+    for e in connections:
+        entry = dict(e)
+
+        # Convert IP addresses and ports of every entry to decimal
+        entry['local_address'] = hex_ip_to_decimal(entry['local_address'])
+        entry['local_port'] = int(entry['local_port'], 16)
+        entry['rem_address'] = hex_ip_to_decimal(entry['rem_address'])
+        entry['rem_port'] = int(entry['rem_port'], 16)
+
+        # Drop entries with 0.0.0.0 as remote address
+        # netstat also does not show these entries
+        if entry['proto'] == 'tcp4' or entry['proto'] == 'udp4':
+            if entry['rem_address'] == '0.0.0.0':
+                # Skip
+                continue
+        if entry['proto'] == 'tcp6' or entry['proto'] == 'udp6':
+            if entry['rem_address'] == '0000:0000:0000:0000:0000:0000:0000:0000':
+                # Skip
+                continue
+
+        # Convert state from hex integer to string
+        state = 'UNKNOWN'
+        state_id = int(entry['st'], 16)
+        if state_id < len(STATE_MAP):
+            state = STATE_MAP[state_id]
+        entry['st'] = state
+
+
+        output.append(entry)
 
     return output
 
-def count_connections(data_list): # type: (list) -> dict
+def count_connections(connections): # type: (list) -> dict
     """
     Count connections in given list of dictionaries by connection protocol
     and connection state.
 
-    :param data_list: List of dictionaries containing info from netstat
-    :raises Exception: If protocol column or state column could not be found
+    :param connections: List of dictionaries containing info about connections
+    :raises Exception: If protocol value or state value could not be found
         in dictionaries
     :return: Dictionary containing number of connections in given state
         categorized by connection protocol.
@@ -225,29 +292,16 @@ def count_connections(data_list): # type: (list) -> dict
 
     # Data entries are guaranteed to all have the same keys, so we
     # just need to check in one of them.
-    if 'State' not in data_list[0]:
-        raise Exception('Could not find column "State" in netstat output')
-    if 'Proto' not in data_list[0]:
-        raise Exception('Could not find column "Proto" in netstat output')
+    if 'st' not in connections[0]:
+        raise Exception('Could not find state entry "st" in connection entry')
+    if 'proto' not in connections[0]:
+        raise Exception('Could not find protocol entry "proto" in connection entry')
 
-    supported_states = [
-        'ESTABLISHED',
-        'SYN_SENT',
-        'SYN_RECV',
-        'FIN_WAIT1',
-        'FIN_WAIT2',
-        'TIME_WAIT',
-        'CLOSE',
-        'CLOSE_WAIT',
-        'LAST_ACK',
-        'LISTEN',
-        'CLOSING',
-        'UNKNOWN',
-    ]
-    supported_protocols = ['tcp', 'udp', 'tcp4', 'tcp6', 'udp4', 'udp6']
+    supported_states = set(STATE_MAP.values())
+    supported_protocols = {'tcp4', 'tcp6', 'udp4', 'udp6'}
 
     # Create empty dict for counting
-    #   connections = {
+    #   statistics = {
     #     'tcp4': {
     #       'ESTABLISHED': 0,
     #       'LISTEN': 0,
@@ -260,58 +314,56 @@ def count_connections(data_list): # type: (list) -> dict
     #     },
     #     ...
     #   }
-    connections = {proto: {} for proto in supported_protocols} # type: dict
+    statistics = {proto: {} for proto in supported_protocols} # type: dict
     for proto in supported_protocols:
         for state in supported_states:
-            connections[proto][state] = 0
+            statistics[proto][state] = 0
 
     # Go through all entries and count number of states/protocols
-    for entry in data_list:
-        proto = entry['Proto']
-        state = entry['State']
+    for entry in connections:
+        proto = entry['proto']
+        state = entry['st']
 
         # Skip unknown protocols or connection states
-        if entry['Proto'] not in supported_protocols:
-            print(f'Unsupported protocol: {entry["Proto"]}', file=sys.stderr)
+        if entry['proto'] not in supported_protocols:
+            print(f'Unsupported protocol: {entry["proto"]}', file=sys.stderr)
             continue
-        if entry['State'] not in supported_states:
-            print(f'Unsupported connection state: {entry["State"]}', file=sys.stderr)
+        if entry['st'] not in supported_states:
+            print(f'Unsupported connection state: {entry["st"]}', file=sys.stderr)
             continue
 
         # Count connection
-        connections[proto][state] += 1
+        statistics[proto][state] += 1
 
     # Add v4 and v6 protocols and sum up all protocols
-    connections['all'] = {}
+    statistics['tcp'] = {}
+    statistics['udp'] = {}
+    statistics['all'] = {}
     for state in supported_states:
-        connections['tcp'][state] = connections['tcp4'][state] + connections['tcp6'][state]
-        connections['udp'][state] = connections['udp4'][state] + connections['udp6'][state]
+        statistics['tcp'][state] = statistics['tcp4'][state] + statistics['tcp6'][state]
+        statistics['udp'][state] = statistics['udp4'][state] + statistics['udp6'][state]
 
-        connections['all'][state] = connections['tcp'][state] + connections['udp'][state]
+        statistics['all'][state] = statistics['tcp'][state] + statistics['udp'][state]
 
-    return connections
+    return statistics
 
 def main(): # type: () -> None
     """
     Main function
     """
 
-    if not check_netstat_availability():
-        print('Error: netstat not found or not installed', file=sys.stderr)
-        sys.exit(1)
+    # Read raw connection info
+    tcp4, tcp6, udp4, udp6 = get_raw_connection_data()
 
-    # Run netstat and capture stdout
-    netstat_stdout = run_netstat()
-
-    # Parse output of netstat
-    data_list = parse_netstat_output(netstat_stdout)
-    data_list = post_process_data(data_list)
+    # Parse connection info and post-process it
+    connections = parse_raw_connection_data(tcp4, tcp6, udp4, udp6)
+    connections = post_process_connection_data(connections)
 
     # Count connections in different states for different protocols
-    connections = count_connections(data_list)
+    statistics = count_connections(connections)
 
     # Output as json
-    print(json.dumps(connections, indent=4))
+    print(json.dumps(statistics, indent=4))
 
 if __name__ == '__main__':
     main()
